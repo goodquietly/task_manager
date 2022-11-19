55fc25cb55519440a477a309240330b0e9ef98cb
Aleksei Iurin <19alex85@gmail.com>
2022-11-19 12:03:03 +0300

    devise + turbo fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get 'users', to: 'devise/sessions#new'
+  end
   devise_for :users
+
+  resources :tasks
 
   root 'tasks#index'
 end
