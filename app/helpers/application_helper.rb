7e0f57d1c6d68514d2eabf46e1a687e90edda353
Aleksei Iurin <19alex85@gmail.com>
2022-11-21 00:03:46 +0300

    views and helper edit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def table_tr_color(status)
+    case status
+    when 'created' then 'warning'
+    when 'started' then 'info'
+    when 'finished' then 'success'
+    end
+  end
 end
